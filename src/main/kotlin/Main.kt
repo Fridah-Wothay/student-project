c00c88344ed46c699d195d75115c07efab13ee98
fridah <wothayafridahkamau@gmail.com>
2024-02-08 11:45:22 +0300

    kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var studentNames = "Fridah Kamaau"
+    println(studentNames)
+    var age: Byte = 21
+    println(age)
+    var phoneNumber = 254724315766
+    println(phoneNumber)
+    var kenyanCitizen: Boolean = true
+    println(kenyanCitizen)
+    var weightInKg: Float = 65.2F
+    println(weightInKg)
+
+
+
+
+
+
+
+
+}
